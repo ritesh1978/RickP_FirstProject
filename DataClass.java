ccb27a180213f6c05418de9b231b332345c3e53c
ritesh1978 <rickpatel1978@gmail.com>
2015-06-17 21:15:29 -0500

    Initial Commit
    Initial Commit

---
+++ DataClass.java
@@ -0,0 +1,74 @@
+package com.dataOperator;
+
+public class DataClass {
+	
+	public static void main(String[] args){
+		dataOperator();
+		PrimitiveDataType();
+		
+	}
+
+	
+// Datatype
+	
+	public static void dataOperator(){
+		
+		int value1 =20;
+		int value2=10;
+		
+		if (value1==value2);
+			System.out.println("value1==value2");
+			
+		
+		
+		if (value1<=value2);
+			System.out.println("value1<=value2");
+			
+			if (value1>value2);
+				
+				System.out.println("Value1>value2");
+				
+				if(value1!=value2);
+					
+				System.out.println("Value1!=value2");
+				
+				}
+			
+		
+		
+	
+	
+		
+				public static void PrimitiveDataType(){
+					
+					int number1 = 11;
+					
+					double number2 =11.22;
+					
+					boolean True = true;
+					boolean False= false;
+					
+					System.out.println("PrimitiveDataType" +number1 + number2);
+					
+					
+					
+					
+					
+					
+				}
+				
+			
+			
+		
+		
+
+
+
+
+	}
+	
+	
+	
+	
+	
+
